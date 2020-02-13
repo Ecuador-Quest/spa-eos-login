97bf5b804f4ebb2ce2a6bc73be1ef28664cfdab9
Leonardo Medina <leoz.31@hotmail.com>
2020-02-13 06:18:16 -0500

    test app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { AuthComponent } from './auth/auth.component';
+
+
+const routes: Routes = [
+  { path: '**', component: AuthComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
+})
+export class AppRoutingModule { }
