acba362b5cf4432567c3a8ca189f27be76f35323
Leonardo Medina <leoz.31@hotmail.com>
2020-02-15 16:12:14 -0500

    tetsss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {AuthModule} from './auth/auth.module';
-/**
- * Auth Module
- */
+
 @NgModule({
   declarations: [
     AppComponent
